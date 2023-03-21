7b1dad467370d0700d1b9bcacdff44eb5eb797f3
Liam Black <blackl@allegheny.edu>
2023-03-21 12:21:53 -0400

    Add column

--- src/main.py
+++ src/main.py
@@ -267,7 +267,7 @@ def predictive_player_data(df2):
     # Select one or more teams from dropdown list
     sorted_unique_team = sorted(df2.TEAM.unique())
     teams_option = ['All Teams'] + sorted_unique_team
-    selected_teams = col5.multiselect('Team', teams_option, default='All Teams')
+    selected_teams = st.multiselect('Team', teams_option, default='All Teams')
 
     try:
         # Select appropriate data based on selected team(s)
@@ -282,18 +282,18 @@ def predictive_player_data(df2):
         return
 
     players = df_selected_teams['Player'].unique()
-    selected_player = col5.selectbox("Select player", players)
+    selected_player = col4.selectbox("Select player", players)
     filtered_data_players = df_selected_teams[df_selected_teams['Player'] == selected_player]
 
     # Select X and Y axes for scatter plot
     # exlude unnecessary collumns (ones that contain stringified dates)
     exclude_cols = ['TEAM', 'Player', 'YEAR']
     y_axis_options = [col for col in filtered_data_players.columns if col not in exclude_cols]
-    y_axis_val = col4.selectbox('Select the Y-axis', options=y_axis_options)
+    y_axis_val = col3.selectbox('Select the Y-axis', options=y_axis_options)
 
     # Create scatter plot with linear regression trendline
     plot = px.scatter(filtered_data_players, x='YEAR', y=y_axis_val, color=filtered_data_players.TEAM, trendline='ols',
-                    trendline_color_override='green', hover_name="TEAM", hover_data=["YEAR", "W"])
+                    trendline_color_override='green', hover_name="TEAM", hover_data=["YEAR"])
 
     # Compute and display R-squared value
     y = df_selected_teams[y_axis_val]
